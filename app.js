2d606f15acd2422f6d248390b9540f8285c89ac7
kingxfsame <artur.kexpa@gmail.com>
2019-09-16 09:52:54 +0300

    localstorage

--- app.js
+++ app.js
@@ -10,7 +10,10 @@ const filter = document.querySelector("#filter");
 loadEventListener();
 
 function loadEventListener(){
+
     form.addEventListener('submit',addTask);
+
+    tasklist.addEventListener('click', removeTask)
 }
 
 function addTask(e){
@@ -22,5 +25,37 @@ function addTask(e){
     deleteLink.innerHTML = '<i class = "fa fa-remove">';
     li.appendChild(deleteLink);
     tasklist.appendChild(li);
+
+    storeTaskInLocalStorage(taskinput.value);
+
+
     e.preventDefault();
+}
+
+function storeTaskInLocalStorage(task){
+    let tasks;
+
+    if(localStorage.getItem('tasks') === null){
+        tasks = [];
+    }
+    else{
+        tasks = JSON.parse(localStorage.getItem('tasks'));
+    }
+    tasks.push(task);
+    localStorage.setItem('tasks',JSON.stringify(tasks));
+    for(var i in tasks)
+    {
+        console.log(tasks[i]);
+    }
+}
+
+function removeTask(e){
+    if(e.target.parentElement.classList.contains('delete-item')){
+        console.log(e);
+        console.log(e.target);
+        if(confirm("Are you sure you want to delete the item?")){
+            e.target.parentElement.parentElement.remove();
+        }
+    }
+    
 }
